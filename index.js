c962a3fe8567844f190b391e7f02ae92f7535619
Jaimitha13 <jaimithalk@gmail.com>
2025-06-10 19:08:36 +0530

    Modified the images

--- index.js
+++ index.js
@@ -8,19 +8,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-const form = document.getElementById('contactForm');
-const formMessage = document.getElementById('formMessage');
-
-form.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent page refresh
-
-    // Here you would add your form submission logic (e.g., sending data to server)
-
-    // Show success message
-    formMessage.style.display = 'block';
-    formMessage.className = 'alert alert-success';
-    formMessage.textContent = 'Thank you for contacting me! I will get back to you soon.';
-
-    // Reset the form fields
-    form.reset();
-});
